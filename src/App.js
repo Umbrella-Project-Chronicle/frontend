2b3b13c717d819dee081a0bd14f360e9b3940a93
Eliot <eliotpitman@gmail.com>
2022-09-09 15:40:40 -0400

    adds route components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import chronicleLogo from "./logo.ico";
 import Data from "./components/api.js";
 import SignUp from "./components/forms.js";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
+import Contact from "./components/contact";
 
 function App() {
   return (
     <div className="App-header">
       <header className="App-header">
+        <Router>
+          <div className="App">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/entry">Chronicle Today</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/about" element={<About />}></Route>
+              <Route exact path="/contact" element={<Contact />}></Route>
+            </Routes>
+          </div>
+        </Router>
         <img src={chronicleLogo} className="Chronicle-logo" alt="chronicleLogo" />
         <Data />
         <p>Welcome to Chronicle</p>
