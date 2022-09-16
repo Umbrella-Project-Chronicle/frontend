3738c2bce5e265f9ad8d3b4ec15da8b8cf68374e
Eliot <eliotpitman@gmail.com>
2022-09-16 13:04:20 -0400

    adds background image

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import "./App.css";
-import chronicleLogo from "./logo.ico";
-import Data from "./components/api.js";
-import SignUp from "./components/forms.js";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import FreeText from "./components/freetext";
+import Data from "./Components/api.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FreeText from "./Components/freetext";
+import Home from "./Pages/home.js";
+import chronicleLogo from "../src/logo.ico";
 
 function App() {
   return (
-    <div className="App-header">
-      <header className="App-header">
+    <div>
+      <header
+        className="App-header"
+        style={{
+          backgroundImage: `url(${chronicleLogo})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
         <Router>
-          <div className="App">
-            <ul>
-              <div>
-                <Link to="/entry">Chronicle Today</Link>
-              </div>
-            </ul>
-            <Routes>
-              <Route exact path="/entry" element={<FreeText />}></Route>
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/entry" element={<FreeText />}></Route>t
+          </Routes>
         </Router>
-        <img src={chronicleLogo} className="Chronicle-logo" alt="chronicleLogo" />
         <Data />
-        <p>Welcome to Chronicle</p>
-        <div>
-          <SignUp />
-        </div>
       </header>
     </div>
   );
---
+++ src/Pages/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SignUp from "../Components/Forms/SignUp.js";
+
+function Home() {
+  return (
+    <div>
+      {" "}
+      <p>Welcome to Chronicle</p>
+      <div>
+        <SignUp />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
