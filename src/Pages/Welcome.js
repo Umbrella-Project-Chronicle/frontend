fad31d8167a60f015e3ef3b69ca6e33bcfa4d23c
Eliot <eliotpitman@gmail.com>
2022-09-29 17:54:26 -0400

    adds userInfo get request

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,28 +1,45 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import { useLocation, Link } from "react-router-dom";
+import axios from "axios";
 
 function Welcome() {
+  // const [userInfo, setUserInfo] = useState([]);
+  const [id, setID] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  // const [email, setemailName] = useState("");
+
   const location = useLocation();
 
   const email = location.state.email;
-  const name = location.state.firstName;
 
-  // const userInfo = async (email) => {
-  //   console.log("email", email);
-  //   await fetch("https://localhost:7177/api/users/search/" + email, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   }).then((response) => {
+  useEffect(() => {
+    console.log("executed only once!");
+    fetchUser(email);
+  }, [""]);
+
+  function fetchUser(email) {
+    axios.get(`https://localhost:7177/api/users/search/` + email).then((response) => {
+      console.log(response.data);
+      // setUserInfo(response);
+      setID(response.data.id);
+      setFirstName(response.data.firstName);
+      setLastName(response.data.lastName);
+    });
+  }
+
+  // const user = userInfo.data.map((x) => x);
+
+  // function userJournals() {
+  //   axios.get("https://localhost:7177/api/users/search/").then((response) => {
   //     console.log(response);
   //   });
-  // };
+  // }
 
   const greeting = () => {
-    if (name) {
-      return name;
+    if (firstName) {
+      return firstName;
     }
     if (email) {
       return email;
