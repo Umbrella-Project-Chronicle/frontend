b40b0f70424817766ec3e9ba63b09adefc7dfa04
eliot-pitman-va <ejpit@hotmail.com>
2022-12-08 12:07:52 -0500

    modal feature in getwraps

--- src/components/Forms/GetWraps.js
+++ src/components/Forms/GetWraps.js
@@ -11,7 +11,14 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
-import { Card, Box, Grid, CircularProgress, Toolbar } from "@mui/material";
+import {
+  Card,
+  Box,
+  Grid,
+  CircularProgress,
+  Toolbar,
+  Button,
+} from "@mui/material";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import circularProgressClasses from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
@@ -20,6 +27,8 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { borderColor } from "@mui/system";
 import { useMediaQuery } from "react-responsive";
+import { EditJournal } from "./EditJournal";
+import useStyles from "../../styles";
 
 export const GetWraps = () => {
   const { DateTime } = require("luxon");
@@ -49,6 +58,10 @@ export const GetWraps = () => {
       return `${year}-02-28T23:59:59.00Z`;
     }
   };
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const months = {
     January: {
@@ -123,7 +136,10 @@ export const GetWraps = () => {
 
     return (
       <Grid>
-        <FormControl sx={{ minWidth: 80, borderColor: "white" }} variant="standard">
+        <FormControl
+          sx={{ minWidth: 80, borderColor: "white" }}
+          variant="standard"
+        >
           <InputLabel>Month</InputLabel>
           <Select
             value={month}
@@ -243,30 +259,108 @@ export const GetWraps = () => {
     query: "(max-width: 600px)",
   });
 
-  const mapSize = () => {
-    const desktopSize = {
+  const sizing = () => {
+    const desktop = {
       height: 800,
       width: 1200,
+      className: classes.journalModal,
     };
     const mediumSize = {
       height: 400,
       width: 600,
     };
-    const mobileSize = {
+    const mobile = {
       height: 300,
       width: 300,
       tooltip: {
         x: 40,
         y: 375,
       },
+      className: classes.mobileJournalModal,
     };
     if (isDesktop) {
-      return desktopSize;
+      return desktop;
     } else if (isMobile) {
-      return mobileSize;
+      return mobile;
     } else {
       return mediumSize;
     }
+  };
+
+  const CustomizedDot = (props) => {
+    const { payload } = props;
+    console.log("props", props);
+    console.log("paylod", payload);
+    console.log("open", open);
+
+    if (open) {
+      return (
+        <Box className={sizing().className}>
+          <Box>
+            <Button
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              CLOSE
+            </Button>
+          </Box>
+          <EditJournal journal={payload} />;
+        </Box>
+      );
+    }
+
+    // if (value > 5) {
+    //   return (
+    //     <svg
+    //       x={cx - 10}
+    //       y={cy - 10}
+    //       width={20}
+    //       height={20}
+    //       fill="red"
+    //       viewBox="0 0 1024 1024"
+    //     >
+    //       <path d="M512 1009.984c-274.912 0-497.76-222.848-497.76-497.76s222.848-497.76 497.76-497.76c274.912 0 497.76 222.848 497.76 497.76s-222.848 497.76-497.76 497.76zM340.768 295.936c-39.488 0-71.52 32.8-71.52 73.248s32.032 73.248 71.52 73.248c39.488 0 71.52-32.8 71.52-73.248s-32.032-73.248-71.52-73.248zM686.176 296.704c-39.488 0-71.52 32.8-71.52 73.248s32.032 73.248 71.52 73.248c39.488 0 71.52-32.8 71.52-73.248s-32.032-73.248-71.52-73.248zM772.928 555.392c-18.752-8.864-40.928-0.576-49.632 18.528-40.224 88.576-120.256 143.552-208.832 143.552-85.952 0-164.864-52.64-205.952-137.376-9.184-18.912-31.648-26.592-50.08-17.28-18.464 9.408-21.216 21.472-15.936 32.64 52.8 111.424 155.232 186.784 269.76 186.784 117.984 0 217.12-70.944 269.76-186.784 8.672-19.136 9.568-31.2-9.12-40.096z" />
+    //     </svg>
+    //   );
+    // }
+
+    // return (
+    //   <svg
+    //     x={cx - 10}
+    //     y={cy - 10}
+    //     width={20}
+    //     height={20}
+    //     fill="green"
+    //     viewBox="0 0 1024 1024"
+    //   >
+    //     <path d="M517.12 53.248q95.232 0 179.2 36.352t145.92 98.304 98.304 145.92 36.352 179.2-36.352 179.2-98.304 145.92-145.92 98.304-179.2 36.352-179.2-36.352-145.92-98.304-98.304-145.92-36.352-179.2 36.352-179.2 98.304-145.92 145.92-98.304 179.2-36.352zM663.552 261.12q-15.36 0-28.16 6.656t-23.04 18.432-15.872 27.648-5.632 33.28q0 35.84 21.504 61.44t51.2 25.6 51.2-25.6 21.504-61.44q0-17.408-5.632-33.28t-15.872-27.648-23.04-18.432-28.16-6.656zM373.76 261.12q-29.696 0-50.688 25.088t-20.992 60.928 20.992 61.44 50.688 25.6 50.176-25.6 20.48-61.44-20.48-60.928-50.176-25.088zM520.192 602.112q-51.2 0-97.28 9.728t-82.944 27.648-62.464 41.472-35.84 51.2q-1.024 1.024-1.024 2.048-1.024 3.072-1.024 8.704t2.56 11.776 7.168 11.264 12.8 6.144q25.6-27.648 62.464-50.176 31.744-19.456 79.36-35.328t114.176-15.872q67.584 0 116.736 15.872t81.92 35.328q37.888 22.528 63.488 50.176 17.408-5.12 19.968-18.944t0.512-18.944-3.072-7.168-1.024-3.072q-26.624-55.296-100.352-88.576t-176.128-33.28z" />
+    //   </svg>
+    // );
+  };
+
+  const CustomEditJournal = (props) => {
+    const { payload } = props;
+    console.log("props", props);
+    console.log("paylod", payload);
+    console.log("open", open);
+
+    // if (open) {
+    //   return (
+    //     <Box className={sizing().className}>
+    //       <Box>
+    //         <Button
+    //           onClick={() => {
+    //             handleClose();
+    //           }}
+    //         >
+    //           CLOSE
+    //         </Button>
+    //       </Box>
+    //       <EditJournal journal={payload} />;
+    //     </Box>
+    //   );
+    // }
   };
 
   // const backgroundcolor = () => {
@@ -279,50 +373,77 @@ export const GetWraps = () => {
 
   return (
     <Grid sx={{ m: 4 }}>
+      <Grid>{open ? CustomizedDot() : <></>}</Grid>
       {averages ? (
         <Grid spacing={2}>
           <Grid xs={10} sm={6} md={6} sx={{ p: 5 }} maxWidth="450px">
             <TableContainer component={Paper}>
               <Table>
                 <TableHead>
-                  <StyledTableCell style={{ fontSize: 20 }}>Emotion</StyledTableCell>
+                  <StyledTableCell style={{ fontSize: 20 }}>
+                    Emotion
+                  </StyledTableCell>
                   <StyledTableCell>{selectMonth()}</StyledTableCell>
                 </TableHead>
                 <TableBody>
                   <StyledTableRow>
                     <StyledTableCell>Overall</StyledTableCell>
                     <StyledTableCell>
-                      {averages.overall ? averages.overall.toFixed(2) : <CircularProgress />}
+                      {averages.overall ? (
+                        averages.overall.toFixed(2)
+                      ) : (
+                        <CircularProgress />
+                      )}
                     </StyledTableCell>
                   </StyledTableRow>
                   <StyledTableRow>
                     <StyledTableCell>Sadness</StyledTableCell>
                     <StyledTableCell>
-                      {averages.sadness ? averages.sadness.toFixed(2) : <CircularProgress />}
+                      {averages.sadness ? (
+                        averages.sadness.toFixed(2)
+                      ) : (
+                        <CircularProgress />
+                      )}
                     </StyledTableCell>
                   </StyledTableRow>
                   <StyledTableRow>
                     <StyledTableCell>Depression</StyledTableCell>
                     <StyledTableCell>
-                      {averages.depression ? averages.depression.toFixed(2) : <CircularProgress />}
+                      {averages.depression ? (
+                        averages.depression.toFixed(2)
+                      ) : (
+                        <CircularProgress />
+                      )}
                     </StyledTableCell>
                   </StyledTableRow>
                   <StyledTableRow>
                     <StyledTableCell>Happiness</StyledTableCell>
                     <StyledTableCell>
-                      {averages.happiness ? averages.happiness.toFixed(2) : <CircularProgress />}
+                      {averages.happiness ? (
+                        averages.happiness.toFixed(2)
+                      ) : (
+                        <CircularProgress />
+                      )}
                     </StyledTableCell>
                   </StyledTableRow>
                   <StyledTableRow>
                     <StyledTableCell>Loneliness</StyledTableCell>
                     <StyledTableCell>
-                      {averages.loneliness ? averages.loneliness.toFixed(2) : <CircularProgress />}
+                      {averages.loneliness ? (
+                        averages.loneliness.toFixed(2)
+                      ) : (
+                        <CircularProgress />
+                      )}
                     </StyledTableCell>
                   </StyledTableRow>
                   <StyledTableRow>
                     <StyledTableCell>Sadness</StyledTableCell>
                     <StyledTableCell>
-                      {averages.sadness ? averages.sadness.toFixed(2) : <CircularProgress />}
+                      {averages.sadness ? (
+                        averages.sadness.toFixed(2)
+                      ) : (
+                        <CircularProgress />
+                      )}
                     </StyledTableCell>
                   </StyledTableRow>
                 </TableBody>
@@ -332,17 +453,11 @@ export const GetWraps = () => {
 
           <Grid>
             <LineChart
-              height={mapSize().height}
-              width={mapSize().width}
+              height={sizing().height}
+              width={sizing().width}
               data={journals}
-              onClick={() => {
-                console.log("do later");
-              }}
-              // onMouseEnter={() => {
-              //   setBackgroundColorTrigger(!backgroundColorTrigger)
-              // }}
-              // onMouseLeave={() => {
-              //   setBackgroundColorTrigger(!backgroundColorTrigger)
+              // onClick={() => {
+              //   console.log("do later");
               // }}
             >
               <Line
@@ -352,12 +467,49 @@ export const GetWraps = () => {
                 dataKey="ratings.overall"
                 stroke="white"
                 strokeWidth={2}
+                dot={<CustomizedDot />}
+                activeDot={{
+                  onClick: () => {
+                    handleOpen();
+                  },
+                }}
               />
-              <Line name="anxiety" type="monotone" dataKey="ratings.anxiety" stroke="#ab0202" strokeWidth={2} />
-              <Line name="loneliness" type="monotone" dataKey="ratings.loneliness" stroke="#038007" strokeWidth={2} />
-              <Line name="depression" type="monotone" dataKey="ratings.depression" stroke="#026969" strokeWidth={2} />
-              <Line name="happiness" type="monotone" dataKey="ratings.happiness" stroke="#f5c402" strokeWidth={2} />
-              <Line name="sadness" type="monotone" dataKey="ratings.sadness" stroke="#020ff5" strokeWidth={2} />
+              <Line
+                name="anxiety"
+                type="monotone"
+                dataKey="ratings.anxiety"
+                stroke="#ab0202"
+                strokeWidth={2}
+                dot={<CustomizedDot />}
+              />
+              <Line
+                name="loneliness"
+                type="monotone"
+                dataKey="ratings.loneliness"
+                stroke="#038007"
+                strokeWidth={2}
+              />
+              <Line
+                name="depression"
+                type="monotone"
+                dataKey="ratings.depression"
+                stroke="#026969"
+                strokeWidth={2}
+              />
+              <Line
+                name="happiness"
+                type="monotone"
+                dataKey="ratings.happiness"
+                stroke="#f5c402"
+                strokeWidth={2}
+              />
+              <Line
+                name="sadness"
+                type="monotone"
+                dataKey="ratings.sadness"
+                stroke="#020ff5"
+                strokeWidth={2}
+              />
 
               <Legend
                 verticalAlign="bottom"
@@ -395,9 +547,9 @@ export const GetWraps = () => {
                   },
                 ]}
               />
-              <XAxis dataKey="date" />
+              <XAxis dataKey={month} />
 
-              <Tooltip isAnimationActive={true} position={mapSize().tooltip} />
+              <Tooltip isAnimationActive={true} position={sizing().tooltip} />
             </LineChart>
           </Grid>
         </Grid>
